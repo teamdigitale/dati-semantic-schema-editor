ca48fd433664ffa9e5a2ea0ab2f7b607d05b6b1e
Roberto Polli <robipolli@gmail.com>
2024-09-26 18:06:26 +0200

    Fix: #34. Example in accordion. TODO: Fix style. (#92)
    * Fix: #34. Example in accordion. TODO: Fix style.
    
    * fix example block playground button
    
    ---------
    
    Co-authored-by: Stefano Baruzzo <stefano.baruzzo@faberbee.com>

--- packages/schema-editor/src/plugins/json-schema-5/components/common/example-block.tsx
+++ packages/schema-editor/src/plugins/json-schema-5/components/common/example-block.tsx
@@ -1,5 +1,6 @@
-import { Callout, CalloutTitle, Icon, CalloutText } from 'design-react-kit';
+import { Icon, Accordion, AccordionItem, AccordionHeader, AccordionBody } from 'design-react-kit';
 import type { Map } from 'immutable';
+import { useState } from 'react';
 
 interface Props {
   depth: number;
@@ -10,6 +11,7 @@ interface Props {
 
 export function ExampleBlock({ depth, schema, jsonldContext, getConfigs }: Props) {
   const { jsonldPlaygroundUrl } = getConfigs();
+  const [isOpened, setIsOpened] = useState(false);
 
   const example = schema.get('example');
   if (!example) {
@@ -17,37 +19,42 @@ export function ExampleBlock({ depth, schema, jsonldContext, getConfigs }: Props
   }
   const exampleString: string = JSON.stringify(example, null, 2);
 
-  const OpenInPlaygroundButton = ({ className }) =>
+  const OpenInPlaygroundButton = () =>
     jsonldPlaygroundUrl && jsonldContext ? (
       <a
-        className={className}
         target="_blank"
         rel="noreferrer"
+        style={{ color: 'inherit' }}
+        onClick={(evt) => evt.stopPropagation()}
         href={
           jsonldPlaygroundUrl +
-          encodeURIComponent(JSON.stringify(
-            {
+          encodeURIComponent(
+            JSON.stringify({
               '@context': jsonldContext.toJSON(),
-              ...(schema?.get('x-jsonld-type') ? {'@type': schema.get('x-jsonld-type')} : {}),
-              ...example.toJSON()
-            }
-          ))
+              ...(schema?.get('x-jsonld-type') ? { '@type': schema.get('x-jsonld-type') } : {}),
+              ...example.toJSON(),
+            }),
+          )
         }
       >
-        <Icon icon="it-external-link" size="sm" title="Open in playground" />
+        <Icon icon="it-external-link" size="sm" title="Open in playground" fill="currentColor" className="ms-2 mb-1" />
       </a>
     ) : null;
 
   return depth <= 1 ? (
-    <Callout color="note" detailed className="p-4 mt-3">
-      <CalloutTitle>
-        Example:
-        <OpenInPlaygroundButton className="ms-auto me-4 z-1" />
-      </CalloutTitle>
-      <CalloutText>
-        <pre className="m-0">{exampleString}</pre>
-      </CalloutText>
-    </Callout>
+    <Accordion background="active">
+      <AccordionItem className="mt-3">
+        <AccordionHeader active={isOpened} onToggle={() => setIsOpened(!isOpened)}>
+          Example
+          <OpenInPlaygroundButton />
+        </AccordionHeader>
+        <AccordionBody active={isOpened}>
+          <small>
+            <pre className="m-0">{exampleString}</pre>
+          </small>
+        </AccordionBody>
+      </AccordionItem>
+    </Accordion>
   ) : (
     <div className="json-schema-2020-12-keyword json-schema-2020-12-keyword--example">
       <span className="json-schema-2020-12-keyword__name json-schema-2020-12-keyword__name--secondary m-0">
