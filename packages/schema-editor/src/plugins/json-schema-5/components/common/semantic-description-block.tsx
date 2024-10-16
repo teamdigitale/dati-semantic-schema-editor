fc3a8f9e76c12b39d8320734cdac2ec09d17ede7
Roberto Polli <robipolli@gmail.com>
2024-10-16 15:02:41 +0200

    Fix: #136. Limit rdfs:comment to 64 chars. (#148)

--- packages/schema-editor/src/plugins/json-schema-5/components/common/semantic-description-block.tsx
+++ packages/schema-editor/src/plugins/json-schema-5/components/common/semantic-description-block.tsx
@@ -3,9 +3,12 @@ import './semantic-description-block.scss';
 export function SemanticDescriptionBlock({ getComponent, description }) {
   const Markdown = getComponent('Markdown', true);
 
+  // Truncate the description to 64 characters
+  const truncatedDescription = description?.length > 64 ? description.substring(0, 64) + '...' : description;
+
   return (
     <span className="rdf-ontological-class-property bg-primary badge">
-      <Markdown source={description} />
+      <Markdown source={truncatedDescription} />
     </span>
   );
 }
