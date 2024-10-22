118a737ee43dff6cebf48cf8bfe1cad04e5ab6a4
Stefano Baruzzo <stefano.baruzzo@faberbee.com>
2024-10-22 19:02:04 +0200

    fix #155 updated vite and tsconfig emit declarations map (#156)
    * fix #155 updated vite and tsconfig emit declarations map
    ---------
    
    Co-authored-by: Roberto Polli <robipolli@gmail.com>

--- packages/schema-editor/vite.config.ts
+++ packages/schema-editor/vite.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, './src/index.ts'),
