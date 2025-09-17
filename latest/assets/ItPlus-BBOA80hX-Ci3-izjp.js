61a07ed3ed31aeb9a93804411ca4b97ae5dc617a
ioggstream <ioggstream@users.noreply.github.com>
2025-09-17 09:33:27 +0000

    deploy: 8c2724748e608cc4846cc0d1c7e7f6a542629dff

---
+++ latest/assets/ItPlus-BBOA80hX-Ci3-izjp.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-CudTW_xV.js";const n=({title:t,titleId:l,...a})=>e.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",xmlSpace:"preserve",enableBackground:"new 0 0 24 24","aria-labelledby":l,...a},t?e.createElement("title",{id:l},t):null,e.createElement("path",{d:"M20 12.5h-7.5V20h-1v-7.5H4v-1h7.5V4h1v7.5H20v1z"}));export{n as component};
