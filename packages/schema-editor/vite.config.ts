36c01407aefbafd85e6b2e437fdf500a22c2c024
Stefano Baruzzo <stefano.baruzzo@par-tec.it>
2025-11-18 10:42:47 +0100

    Fix: #61. Help section fails unless helpmd is manually imported locally (#63)
    * help.md bundled as static asset
    
    * changeset

--- packages/schema-editor/vite.config.ts
+++ packages/schema-editor/vite.config.ts
@@ -6,6 +6,7 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts({ insertTypesEntry: true })],
+  assetsInclude: ['**/*.md'],
   build: {
     lib: {
       entry: path.resolve(__dirname, './src/index.ts'),
