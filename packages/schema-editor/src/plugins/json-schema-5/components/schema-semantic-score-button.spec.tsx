0555be3215ddb78d7b66785471294635e6c77558
Stefano Baruzzo <stefano.baruzzo@par-tec.it>
2025-12-04 17:38:41 +0100

    fixed unit tests

--- packages/schema-editor/src/plugins/json-schema-5/components/schema-semantic-score-button.spec.tsx
+++ packages/schema-editor/src/plugins/json-schema-5/components/schema-semantic-score-button.spec.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import * as configuration from '../../configuration';
-import * as utils from '../utils';
+import * as utils from '@teamdigitale/schema-editor-utils';
 import { SchemaSemanticScoreButton } from './schema-semantic-score-button';
 
 describe('SchemaSemanticScoreButton', () => {
