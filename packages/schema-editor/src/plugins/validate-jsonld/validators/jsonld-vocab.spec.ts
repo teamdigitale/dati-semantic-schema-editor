eee22aa3504a90e59f05a23d8d349a85d0e0aed0
Stefano Baruzzo <stefano.baruzzo@par-tec.it>
2026-01-15 16:58:46 +0100

    jsonld @id values check, converged jsonld property resolution

--- packages/schema-editor/src/plugins/validate-jsonld/validators/jsonld-vocab.spec.ts
+++ packages/schema-editor/src/plugins/validate-jsonld/validators/jsonld-vocab.spec.ts
@@ -1,4 +1,5 @@
 import { fromJS, OrderedMap } from 'immutable';
+import yaml from 'js-yaml';
 import { describe, expect, it, vi } from 'vitest';
 import { validateJsonldVocab } from './jsonld-vocab';
 
@@ -17,48 +18,40 @@ const createMockSystem = (specJson: OrderedMap<string, any>) => {
 describe('validateJsonldVocab', () => {
   describe('validate jsonld context', () => {
     it('should not report errors for valid jsonld context', async () => {
-      const specJson = fromJS({
-        components: {
-          schemas: {
-            Person: {
-              'x-jsonld-context': {
-                '@base': 'https://example.com/',
-                '@vocab': 'https://vocab.example.com/',
-                name: 'name',
-              },
-              properties: {
-                name: {
-                  type: 'string',
-                },
-              },
-            },
-          },
-        },
-      });
+      const specYaml = `openapi: 3.0.3
+components:
+  schemas:
+    Person:
+      type: object
+      x-jsonld-context:
+        '@base': 'https://example.com/'
+        '@vocab': 'https://vocab.example.com/'
+        name: 'name'
+      properties:
+        name:
+          type: string
+        `;
+      const specJson = fromJS(yaml.load(specYaml));
       const system = createMockSystem(specJson);
       const errors = await validateJsonldVocab(system);
       expect(errors).toHaveLength(0);
     });
 
     it('should report error for invalid jsonld context', async () => {
-      const specJson = fromJS({
-        components: {
-          schemas: {
-            Person: {
-              'x-jsonld-context': {
-                '@base': 'https://example.com/',
-                '@vocab': 'https://vocab.example.com/',
-                invalid: 'invalid:iri',
-              },
-              properties: {
-                invalid: {
-                  type: 'string',
-                },
-              },
-            },
-          },
-        },
-      });
+      const specYaml = `openapi: 3.0.3
+components:
+  schemas:
+    Person:
+      type: object
+      x-jsonld-context:
+        '@base': 'https://example.com/'
+        '@vocab': 'https://vocab.example.com/'
+        invalid: 'invalid:iri'
+      properties:
+        invalid:
+          type: string
+        `;
+      const specJson = fromJS(yaml.load(specYaml));
       const system = createMockSystem(specJson);
       const errors = await validateJsonldVocab(system);
       expect(errors.length).toBeGreaterThan(0);
@@ -67,53 +60,101 @@ describe('validateJsonldVocab', () => {
   });
 
   describe('validate @id property', () => {
-    it('should warn when integer property is associated with @id', async () => {
-      const specJson = fromJS({
-        components: {
-          schemas: {
-            Foo: {
-              'x-jsonld-context': {
-                '@base': 'https://example.com/',
-                foo: '@id',
-              },
-              properties: {
-                foo: {
-                  type: 'integer',
-                },
-              },
-            },
-          },
-        },
-      });
+    it('should warn when nested integer property is associated with @id', async () => {
+      const specYaml = `openapi: 3.0.3
+components:
+  schemas:
+    Foo:
+      type: object
+      x-jsonld-context:
+        '@base': 'https://example.com/'
+        foo: '@id'
+      properties:
+        foo:
+          type: integer
+          
+    BirthPlace:
+      type: object
+      properties:
+        code:
+          type: integer
+          example: 100
+        name:
+          type: string
+    
+    Person:
+      type: object
+      x-jsonld-type: https://w3id.org/italia/onto/CPV/Person
+      x-jsonld-context:
+        '@vocab': 'https://w3id.org/italia/onto/CPV/'
+        given_name: givenName
+        birth_place:
+          '@context':
+            "@vocab": 'https://example.com/'
+            code: "@id"
+            name: name
+      properties:
+        given_name:
+          maxLength: 255
+          type: string
+        birth_place:
+          $ref: '#/components/schemas/BirthPlace'
+          `;
+      const specJson = fromJS(yaml.load(specYaml));
       const system = createMockSystem(specJson);
       const errors = await validateJsonldVocab(system);
-      expect(errors.length).toBeGreaterThan(0);
-      const warning = errors.find((e) => e.level === 'warning' && e.message.includes('scalar property'));
-      expect(warning).toBeDefined();
-      expect(warning?.path).toEqual(['components', 'schemas', 'Foo', 'properties', 'foo']);
+      const warning = errors.filter(
+        (e) => e.level === 'warning' && e.message.includes('The @id annotation should be used with string properties.'),
+      );
+      expect(warning.length).toEqual(2);
     });
 
     it('should not warn when string property is associated with @id', async () => {
-      const specJson = fromJS({
-        components: {
-          schemas: {
-            Foo: {
-              'x-jsonld-context': {
-                '@base': 'https://example.com/',
-                foo: '@id',
-              },
-              properties: {
-                foo: {
-                  type: 'string',
-                },
-              },
-            },
-          },
-        },
-      });
+      const specYaml = `openapi: 3.0.3
+components:
+  schemas:
+    Foo:
+      type: object
+      x-jsonld-context:
+        '@base': 'https://example.com/'
+        foo: '@id'
+      properties:
+        foo:
+          type: string
+          
+    BirthPlace:
+      type: object
+      properties:
+        code:
+          type: string
+          example: 'A001'
+        name:
+          type: string
+    
+    Person:
+      type: object
+      x-jsonld-type: https://w3id.org/italia/onto/CPV/Person
+      x-jsonld-context:
+        '@vocab': 'https://w3id.org/italia/onto/CPV/'
+        given_name: givenName
+        birth_place:
+          '@context':
+            "@vocab": 'https://example.com/'
+            code: "@id"
+            name: name
+      properties:
+        given_name:
+          maxLength: 255
+          type: string
+        birth_place:
+          $ref: '#/components/schemas/BirthPlace'
+          `;
+      const specJson = fromJS(yaml.load(specYaml));
       const system = createMockSystem(specJson);
       const errors = await validateJsonldVocab(system);
-      const warning = errors.find((e) => e.level === 'warning' && e.message.includes('scalar property'));
+      const warning = errors.find(
+        (e) => e.level === 'warning' && e.message.includes('The @id annotation should be used with string properties.'),
+      );
       expect(warning).toBeUndefined();
     });
   });
--- packages/schema-editor/src/plugins/validate-jsonld/validators/jsonld-vocab.ts
+++ packages/schema-editor/src/plugins/validate-jsonld/validators/jsonld-vocab.ts
@@ -1,5 +1,6 @@
+import { resolveJsonldContext, resolvePropertyByJsonldContext } from '@teamdigitale/schema-editor-utils';
 import { Map, OrderedMap } from 'immutable';
-import { expand } from 'jsonld';
+import { resolveSpecPathRefs } from '../../jump-to-path/utils';
 import { SwaggerError } from '../models/error';
 
 /**
@@ -8,47 +9,91 @@ import { SwaggerError } from '../models/error';
  * @returns An array of Swagger Editor's errors.
  */
 export const validateJsonldVocab = async (system): Promise<SwaggerError[]> => {
-  // Get basePath from spec
   const specJson = system.specSelectors.specJson() as OrderedMap<string, any>;
 
-  // Checking for errors recursively
   const source = system.jsonldValidatorSelectors.errSource();
   const errors: SwaggerError[] = [];
 
-  const executeDeeply = async (
-    path: string[],
-    contextPath: string[] | undefined,
-    rootJsonldContext: OrderedMap<string, any> | undefined,
-  ) => {
-    const partialSpec = specJson.getIn(path);
-    if (Map.isMap(partialSpec)) {
-      for (const [key, value] of partialSpec.entries() as IterableIterator<[string, unknown]>) {
-        const innerPath = [...path, key];
-        if (key === 'x-jsonld-context') {
-          await executeDeeply(innerPath, innerPath, value as OrderedMap<string, any>);
-        } else if (contextPath && rootJsonldContext && !['@context', '@vocab', '@base'].includes(key)) {
-          const propertyPath = innerPath.slice(contextPath.length);
-          const input = propertyPath.reduceRight((obj, x, i) => ({ [x]: i < propertyPath.length - 1 ? obj : '' }), {});
-          try {
-            await expand({ '@context': rootJsonldContext.toJS(), ...input });
-          } catch (ex) {
+  // Extract all data models from spec
+  const SCHEMAS_PATH = ['components', 'schemas'];
+  const dataModels = specJson.getIn(SCHEMAS_PATH) as Map<any, any> | undefined;
+  if (!dataModels) {
+    return errors;
+  }
+
+  // Process every element in #/components/schemas that has a jsonld context
+  const filteredSchemas = dataModels.filter((x) => x.has('x-jsonld-context'));
+  for (const [dataModelKey, dataModel] of filteredSchemas.entries()) {
+    // Get the root jsonld context
+    const jsonldContext = resolveJsonldContext(dataModel)?.get('@context');
+    if (!jsonldContext) {
+      continue;
+    }
+
+    // VALIDATION 1:
+    // Validate jsonld context @id values associated with string properties
+    function validateJsonldContextIdValuesDeeply(partialJsonldContext: Map<string, any>, resolvedPath: string[]) {
+      const entries = Array.from(partialJsonldContext.entries());
+
+      for (const [key, value] of entries) {
+        const innerPath = [...resolvedPath, key];
+
+        // Process nested values
+        if (Map.isMap(value)) {
+          validateJsonldContextIdValuesDeeply(value as Map<string, any>, innerPath);
+        }
+
+        // If the value equals "@id" then check for object property type
+        else if (value === '@id') {
+          const propertyPath = resolveSpecPathRefs(system, [
+            ...SCHEMAS_PATH,
+            dataModelKey,
+            'properties',
+            ...innerPath.map((x) => (x === '@context' ? 'properties' : x)),
+          ]);
+          const property = specJson.getIn(propertyPath) as Map<string, any>;
+          if (!property || property.get('type') !== 'string') {
+            const jsonldPropertyFullPath = [...SCHEMAS_PATH, dataModelKey, 'x-jsonld-context', ...innerPath];
             errors.push({
               type: 'spec',
               source,
-              level: 'error',
-              message: ex.message,
-              path: innerPath,
-              line: system.specSelectors.getSpecLineFromPath(innerPath),
+              level: 'warning',
+              message: `The @id annotation should be used with string properties.`,
+              path: jsonldPropertyFullPath,
+              line: system.specSelectors.getSpecLineFromPath(jsonldPropertyFullPath),
             });
           }
-        } else {
-          await executeDeeply(innerPath, contextPath, rootJsonldContext);
         }
       }
     }
-  };
+    validateJsonldContextIdValuesDeeply(jsonldContext, []);
 
-  await executeDeeply(['components', 'schemas'], undefined, undefined);
+    // VALIDATION 2:
+    // Check all object properties are valid in the jsonld context
+    const propertiesPaths: string[][] =
+      dataModel
+        .get('properties')
+        ?.keySeq()
+        .toArray()
+        .map((x) => [x]) || [];
+
+    for (const propertyRelativePath of propertiesPaths) {
+      try {
+        await resolvePropertyByJsonldContext(jsonldContext, propertyRelativePath);
+      } catch (ex) {
+        const propertyFullPath = [...SCHEMAS_PATH, dataModelKey, 'properties', ...propertyRelativePath];
+        const propertyLine = system.specSelectors.getSpecLineFromPath(propertyFullPath);
+        errors.push({
+          type: 'spec',
+          source,
+          level: 'error',
+          message: ex.message,
+          path: propertyFullPath,
+          line: propertyLine,
+        });
+      }
+    }
+  }
 
   return errors;
 };
