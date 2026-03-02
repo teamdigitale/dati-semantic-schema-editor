8a7872d2c99ff0fcdb802a030e09186f03a4f380
Stefano Baruzzo <stefano.baruzzo@par-tec.it>
2026-03-02 11:47:58 +0100

    removed async state type

--- packages/schema-editor/src/plugins/json-schema-5/models/async-state.ts
+++ packages/schema-editor/src/plugins/json-schema-5/models/async-state.ts
@@ -1,5 +1,3 @@
-export type AsyncStateStatus = 'idle' | 'pending' | 'fulfilled' | 'error';
-
 export type AsyncState<DataType, ErrorType = string> =
   | { status: 'idle'; data?: undefined; error?: undefined }
   | { status: 'pending'; data?: undefined; error?: undefined }
