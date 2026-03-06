be3bf7431a52792e4f4867bd707b92de3188bfe5
stefanone91 <stefanone91@users.noreply.github.com>
2026-03-06 09:14:46 +0000

    deploy: 0d61d4d44e9896995c5768a561bad2534bdf44f5

---
+++ stefanone91-127-missing-contactsname-in-publiccodeyml/config.js
@@ -0,0 +1,5 @@
+window.__ENV = {
+  sparqlUrl: 'https://virtuoso-test-external-service-ndc-test.apps.cloudpub.testedev.istat.it/sparql',
+  oasCheckerUrl: 'https://italia.github.io/api-oas-checker',
+  schemaEditorUrl: 'https://teamdigitale.github.io/dati-semantic-schema-editor/latest/',
+};
